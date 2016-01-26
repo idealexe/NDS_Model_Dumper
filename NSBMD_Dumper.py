743decb570ed4198ee5ddd55694caa0b1667825a
idealexe <omega_8810@yahoo.co.jp>
2016-01-26 23:00:34 +0900

    Start

--- NSBMD_Dumper.py
+++ NSBMD_Dumper.py
@@ -51,6 +51,7 @@ with open(file, 'rb') as romFile:
 
     fileName = os.path.basename(file)   # ファイルの名前をパスから切り出し
     path, ext = os.path.splitext(fileName)  # ベースネームと拡張子を取得
+
     if os.path.exists(path) != 1:
         os.mkdir(path)  # ベースネームと同じ名前のフォルダを作成
         os.mkdir(path + "\\nsbmd")  # モデル用フォルダ
@@ -64,15 +65,20 @@ with open(file, 'rb') as romFile:
         print match.group()
         #print hex(matchAddr)
 
+        output = "" # 出力データ格納用
         compForm = readBin(data, matchAddr - 5) # 圧縮形式を示すデータは5バイト前
 
         if compForm == str('\x10'): # LZ77 0x10
             print "LZ77(0x10) compression"
             uncompSize = struct.unpack('l', data[matchAddr - 4 : matchAddr])
             uncompSize = uncompSize[0]
+
+            # ファイルサイズが異常ならスキップ
+            if uncompSize <= 0 or int("FFFFFF", 16) < uncompSize:
+                continue
+
             print "Uncompressed Size: " + str(uncompSize) + " Bytes"
 
-            output = "" # 出力用データ
             readPos = matchAddr - 1 # Start
             writePos = 0
 
@@ -128,41 +134,60 @@ with open(file, 'rb') as romFile:
             #print output
             #print len(output)
 
-            # nsbmdファイルなら
-            if match.group() == "BMD0":
-                ext = "nsbmd"
-                # セクション数によって名前の位置がずれる
-                if output[14] == str('\x01'):
-                    outName = output[52:62].translate(None, str('\x00'))
-                else:
-                    outName = output[56:66].translate(None, str('\x00'))
-
-            # nsbtxファイルなら
-            elif match.group() == "BTX0":
-                ext = "nsbtx"
-                # 名前が格納されているブロックの先頭アドレスを計算する
-                # メインヘッダは共通で 80 Bytes
-                blockSize = struct.unpack('h',output[86:88])    # 謎のブロックのサイズ
-                blockSize = blockSize[0]
-                infoSize = struct.unpack('h',output[80+blockSize+2:80+blockSize+4]) # 情報ブロックのサイズ
-                infoSize = infoSize[0]
-                outName = output[80 + blockSize + infoSize:80 + blockSize+ infoSize + 16].translate(None, str('\x00'))
-            else:
-                ext = "unknown"
-
-            with open(path + "\\" + ext + "\\" + outName + "." + ext, "wb") as out:
-                print outName + "." + ext + "\n"
-                out.write(output)
-                #print readPos
-
-            #break   # 1モデルだけ出力
-
-
         elif compForm[0] == str('\x11'):
             print "LZ77(0x11) compression"
             uncompSize = struct.unpack('l', data[matchAddr -4 : matchAddr])
             uncompSize = uncompSize[0]
+
+            # ファイルサイズが異常ならスキップ
+            if uncompSize <= 0 or int("FFFFFF", 16) < uncompSize:
+                continue
+
             print "Uncompressed Size: " + str(uncompSize) + " Bytes"
 
+        # 非圧縮なら
         else:
             print "not compressed"
+            fileSize = struct.unpack('l', data[matchAddr + 8 : matchAddr + 12])
+            fileSize = fileSize[0]
+            # ファイルサイズが0以下だったりLZ77であつかえるサイズ以上ならスキップ
+            if fileSize <= 0 or fileSize > int("FFFFFF",16):
+                continue
+
+            print "File Size: " + str(fileSize) + " Bytes"
+            output = data[matchAddr:matchAddr + fileSize]
+
+        """
+        出力処理
+        """
+        # nsbmdファイルなら
+        if match.group() == "BMD0":
+            ext = "nsbmd"
+            # セクション数によって名前の位置がずれる
+            if output[14] == str('\x01'):
+                outName = output[52:62].translate(None, str('\x00'))
+            else:
+                outName = output[56:66].translate(None, str('\x00'))
+
+        # nsbtxファイルなら
+        elif match.group() == "BTX0":
+            ext = "nsbtx"
+            # 名前が格納されているブロックの先頭アドレスを計算する
+            # メインヘッダは共通で 80 Bytes
+            blockSize = struct.unpack('h',output[86:88])    # 謎のブロックのサイズ
+            blockSize = blockSize[0]
+            infoSize = struct.unpack('h',output[80+blockSize+2:80+blockSize+4]) # 情報ブロックのサイズ
+            infoSize = infoSize[0]
+            outName = output[80 + blockSize + infoSize:80 + blockSize+ infoSize + 16].translate(None, str('\x00'))
+        else:
+            ext = "unknown"
+
+        try:
+            with open(path + "\\" + ext + "\\" + outName + "." + ext, "wb") as out:
+                print outName + "." + ext + "\n"
+                out.write(output)
+                #print readPos
+        except  IOError:
+            print "不正なファイル名のためスキップしました．"
+
+        #break   # 1モデルだけ出力
